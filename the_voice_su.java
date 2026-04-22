688e7781cae7ee9fd6ffa9c8d9cec113c27d0c43
ptd1st <ganmopu4@gmail.com>
2026-04-23 02:43:03 +0700

    Add files via upload

--- the_voice_su.java
+++ the_voice_su.java
@@ -10,11 +10,12 @@ public class the_voice_su {
         int pp1 = 0;
         int pp2 = 0;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             int sex = input.nextInt();
             int score1 = input.nextInt();
             int score2 = input.nextInt();
             if (score1 >= 9 && score2 >= 9) {
+                // เหลือคู่
                 if (pp1 < K && pp2 < K) {
                     if (sex == 1) {
                         pp1++;
@@ -23,26 +24,27 @@ public class the_voice_su {
                         pp2++;
                         System.out.println(i + " " + "2");
                     }
-
-                } else if (pp1 < K) {
+                }
+                // เหลือคน 1
+                else if (pp1 < K) {
                     pp1++;
                     System.out.println(i + " 1");
                 }
-                // เหลือแค่คนที่ 2
+                // เหลือคน 2
                 else if (pp2 < K) {
                     pp2++;
                     System.out.println(i + " 2");
                 }
 
             }
-            // อยากรับแค่คนที่ 1
+            // รับแค่คน 1
             else if (score1 >= 9) {
                 if (pp1 < K) {
                     pp1++;
                     System.out.println(i + " 1");
                 }
             }
-            // อยากรับแค่คนที่ 2
+            // รับแค่คน 2
             else if (score2 >= 9) {
                 if (pp2 < K) {
                     pp2++;
